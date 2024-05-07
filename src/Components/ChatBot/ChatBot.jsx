3e1c1820fac4a50ffa5dc2a79408f663d799e702
KevinJp21 <kevinjp821@gmail.com>
2024-05-07 10:50:38 -0500

    funcionalidad del boton cerrar y abrir chatbot

--- src/Components/ChatBot/ChatBot.jsx
+++ src/Components/ChatBot/ChatBot.jsx
@@ -1,13 +1,24 @@
 import React, {useState} from 'react'
 import './ChatBot.css'
 const ChatBot = () => {
+    const [isOpenChatBot, setIsOpenChatBot] = useState(false);
+
+    const handleOpenChatBot = () => {
+        setIsOpenChatBot(true)
+    }
+
+    const handleCloseChatBot = () => {
+        setIsOpenChatBot(false)
+    }
+
   return (
     <>
         
-        <button className='btnChatBot'>
+        <button className='btnChatBot' onClick={handleOpenChatBot}>
             <span>DocMe ChatBot</span>
         </button>
-    <div className='ChatBotContainer'>
+
+    <div className={`ChatBotContainer ${isOpenChatBot ? 'ChatBotOpen' : ''}`}>
         <div className='ContentChatBot'>
             <div className='HeaderChatBot'>
                 <div className='TitleChatBot'>
@@ -15,7 +26,7 @@ const ChatBot = () => {
                 </div>
 
                 <div className='BTNChatBotCLose'>
-                    <button className='CloseChatBot'>
+                    <button className='CloseChatBot' onClick={handleCloseChatBot}>
                         <i className="fa-solid fa-circle-xmark"></i>
                     </button>
                 </div>
