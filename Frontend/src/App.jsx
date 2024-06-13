d56a87701d158ea45f678c9be20b9d48c98879a7
KevinJp21 <kevinjp821@gmail.com>
2024-06-13 15:35:35 -0500

    Agregando lazy component para cargar componentes cuando se requiera

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,35 @@
-import React, { useContext } from 'react';
+import React, { lazy, Suspense, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from './Components/AuthContext/AuthContext';
-import Dashboard from './Components/Dashboard/Dashboard';
-import LoginForm from './Components/Login/Login';
-import SignUp from './Components/SignUp/SignUp';
-import Main from './Components/Home/Main/Main';
+
+
+const Dashboard = lazy(() => import('./Components/Dashboard/Dashboard'));
+const LoginForm = lazy(() => import('./Components/Login/Login'));
+const SignUp = lazy(() => import('./Components/SignUp/SignUp'));
+const Main = lazy(() => import('./Components/Home/Main/Main'));
 
 function App() {
   const { isLoggedIn, loading, userRole } = useContext(AuthContext);
 
   if (loading) {
-    return <div></div>; // Improved loading indication
+    return <div></div>;
   }
 
-  // Construct the dynamic dashboard route based on user role
+  // Construye la ruta del dashboard basado en el rol
   const dashboardPath = `/DocMe/${userRole}/dashboard`;
 
   return (
     <Router>
-      <Routes>
-        <Route path="/DocMe/login" element={!isLoggedIn ? <LoginForm /> : <Navigate to={dashboardPath} />} />
-        <Route path="/DocMe/signup" element={!isLoggedIn ? <SignUp /> : <Navigate to={dashboardPath} />} />
-        <Route path={dashboardPath} element={isLoggedIn ? <Dashboard /> : <Navigate to="/DocMe/login" />} />
-        <Route path="/DocMe/home" element={<Main />}/>
-        <Route path="*" element={<Navigate to={isLoggedIn ? dashboardPath : "/DocMe/home"} />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/DocMe/login" element={!isLoggedIn ? <LoginForm /> : <Navigate to={dashboardPath} />} />
+          <Route path="/DocMe/signup" element={!isLoggedIn ? <SignUp /> : <Navigate to={dashboardPath} />} />
+          <Route path={dashboardPath} element={isLoggedIn ? <Dashboard /> : <Navigate to="/DocMe/login" />} />
+          <Route path="/DocMe/home" element={<Main />} />
+          <Route path="*" element={<Navigate to={isLoggedIn ? dashboardPath : "/DocMe/home"} />} />
+        </Routes>
+      </Suspense>
+
     </Router>
   );
 }
