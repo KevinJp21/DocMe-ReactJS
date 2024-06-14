bb8981ab1df0441c2f581da09645578a53a40f9b
KevinJp21 <kevinjp821@gmail.com>
2024-06-14 16:46:21 -0500

    .

--- Frontend/src/Components/Routes/PrivateRoute.jsx
+++ Frontend/src/Components/Routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from '../AuthContext/AuthContext';
 const PrivateRoute = ({ children }) => {
     const { isLoggedIn } = useContext(AuthContext);
 
-    return isLoggedIn ? children : <Navigate to="/home" />;
+    return isLoggedIn ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
